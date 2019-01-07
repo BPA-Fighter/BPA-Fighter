8df1c25652d606e29888b285cc14621aa2231bc4
AltonRune <44446219+AltonRune@users.noreply.github.com>
2019-01-07 13:22:22 -0500

    Add files via upload

---
+++ Fight.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bpa.brawler;
+
+/**
+ *
+ * @author Joshua Burton
+ */
+public class Fight {
+//this is for everything that the fighters will do damage with
+    
+public void punch(){
+
+}
+
+public void kick(){
+    
+}
+
+public void block(){
+    
+}
+
+}
+
+
---
+++ startScreen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bpa.brawler;
+
+/**
+ *
+ * @author Joshua Burton
+ */
+public class startScreen {
+//For the start screen/main menue
+    
+ public static void loaded(){
+     
+ } 
+ 
+ 
+}
