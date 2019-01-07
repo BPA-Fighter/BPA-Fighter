a80a6c92bb5e55520c939b505799e542b8e2fe5f
Todd Wesley <hpro1@sv156-imac23.sylvaniaschools.local>
2019-01-07 13:32:56 -0500

    Created Main

---
+++ The-Fight-Of-Time/src/the/fight/of/time/TheFightOfTime.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package the.fight.of.time;
+
+/**
+ *
+ * @author Todd Wesley
+ */
+public class TheFightOfTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
